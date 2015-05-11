12e76b2079316909d8b9ccdabd02d50833ba9489
jdavisonc <jdavisonc@gmail.com>
2015-05-10 21:47:24 -0300

    - Load all csv columns as different charts - Enable to load multiple files to be able to compare it as different series.

--- app.js
+++ app.js
@@ -14,28 +14,69 @@ app.config(function(ChartJsProvider) {
 
 app.controller('MenuCtrl', function($scope) {
     $scope.isCollapsed = true;
-    $scope.charts = ['Line', 'Bar', 'Doughnut', 'Pie', 'Polar Area', 'Radar', 'Base'];
 });
 
 app.controller('LineCtrl', ['$scope', '$timeout', function($scope, $timeout) {
-    $scope.init = function() {
-		var results = Papa.parse("/data.csv", {
-			download: true,
-			complete: function(results) {
-	            var serie = [], labels = [];
-                angular.forEach(results.data, function(row) {
-                    labels.push(row[0]);
-                    serie.push(row[3]);
-                });
+	$scope.charts = {};
+	$scope.series = [ 
+		{ path: "/data/data.csv" }, 
+		{ path: "/data/data2.csv"}, 
+		{ path: "/data/data3.csv"} 
+	];
 
-                $scope.labels = labels;
-                $scope.data = [];
-                $scope.data.push(serie);
-                $scope.$apply();
-			}
+	$scope.addSerie = function() {
+		$scope.series.push({ path: ""});
+	};
+
+	$scope.deleteSerie = function(index) {
+		$scope.series.splice(index, 1);
+	};
+
+    $scope.draw = function() {
+    	$scope.charts = {};
+    	console.log($scope.series);
+    	angular.forEach($scope.series, function(serie) {
+			Papa.parse(serie.path, {
+				header: true,
+				download: true,
+				complete: function(results) {
+		            var datas = [], labels = [];
+		            var fields = results.meta.fields;
+	                
+	                // transform csv data into an series array
+	                angular.forEach(results.data, function(row) {
+	                    labels.push(row[fields[0]]); // field one must be the X axis
+	                    for (var i = 0; i < fields.length - 1; i++) {
+	                    	if (!datas[i]) {
+	                    		datas[i] = [];
+	                    	}
+	                    	datas[i].push(row[fields[i+1]]);
+	                    };
+	                    
+	                });
+
+	                // create chart data-type to hold chart information
+	                var i = 1; // ignoring first column
+	                angular.forEach(datas, function(data) {
+	                	var name = results.meta.fields[i++];
+	                	if (!$scope.charts[name]) {
+		                	$scope.charts[name] = {
+		                		labels: labels,
+		                		data: [ data ],
+		                		series: [ serie.path ],
+		                		title: name
+		                	};
+		                } else {
+		                	$scope.charts[name].data.push(data);
+		                	$scope.charts[name].series.push(serie.path);
+		                }
+	                });
+	                $scope.$apply();
+				}
+			});
 		});
     };
 
-    $scope.init();
+    $scope.draw();
 
 }]);
